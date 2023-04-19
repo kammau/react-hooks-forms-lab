adbce0ffe659f9857d63ce956b4d78b4e7b87120
Kamryn Smith <programersign@gmail.com>
2023-04-19 10:13:42 -0700

    assignment complete

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,15 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleFormSubmit(formData) {
+    setItems([...items, formData])
+    console.log(items)
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleFormSubmit}/>
     </div>
   );
 }
